6f2eb1bb63bfe526f74ca0f5e519164bc4982e8c
hatelove <jay.hatelove@gmail.com>
2017-02-21 21:33:55 +0800

    add the first failed test case

---
+++ LeetCode_15_3Sum/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using ExpectedObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace LeetCode_15_3Sum
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var nums = new int[] { -1, 0, 1, 2, -1, -4 };
+
+            var expected = new List<List<int>>
+            {
+                new List<int> {-1, 0, 1},
+                new List<int> {-1, -1, 2},
+            };
+
+            var actual = new Solution().ThreeSum(nums);
+
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+    }
+
+    public class Solution
+    {
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            return null;
+        }
+    }
+}

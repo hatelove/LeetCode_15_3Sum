8359a30bb07bacaa941c7042de6007cfed62aca7
hatelove <jay.hatelove@gmail.com>
2017-02-21 21:37:43 +0800

    add the simplest failed test: nums={0,0,0}

--- LeetCode_15_3Sum/UnitTest1.cs
+++ LeetCode_15_3Sum/UnitTest1.cs
@@ -20,12 +20,25 @@ namespace LeetCode_15_3Sum
                 new List<int> {-1, -1, 2},
             };
 
+            ShouldBeEqual(nums, expected);
+        }
+
+        private static void ShouldBeEqual(int[] nums, List<List<int>> expected)
+        {
             var actual = new Solution().ThreeSum(nums);
 
             expected.ToExpectedObject().ShouldEqual(actual);
         }
 
+        [TestMethod]
+        public void test_nums_0_0_0()
+        {
+            var nums = new int[] { 0, 0, 0 };
+            var expected = new List<List<int>>
+                {new List<int>() {0, 0, 0}};
 
+            ShouldBeEqual(nums, expected);
+        }
     }
 
     public class Solution

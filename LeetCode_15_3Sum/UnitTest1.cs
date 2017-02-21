6b5a4ae40525f2f12f333f64898efc0cbb5c3abe
hatelove <jay.hatelove@gmail.com>
2017-02-21 21:41:16 +0800

    pass the first test case:by 3 for loops

--- LeetCode_15_3Sum/UnitTest1.cs
+++ LeetCode_15_3Sum/UnitTest1.cs
@@ -1,6 +1,7 @@
 ﻿using ExpectedObjects;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 
 namespace LeetCode_15_3Sum
 {
@@ -14,7 +15,7 @@ namespace LeetCode_15_3Sum
         {
             var nums = new int[] { -1, 0, 1, 2, -1, -4 };
 
-            var expected = new List<List<int>>
+            var expected = new List<IList<int>>
             {
                 new List<int> {-1, 0, 1},
                 new List<int> {-1, -1, 2},
@@ -23,7 +24,7 @@ namespace LeetCode_15_3Sum
             ShouldBeEqual(nums, expected);
         }
 
-        private static void ShouldBeEqual(int[] nums, List<List<int>> expected)
+        private static void ShouldBeEqual(int[] nums, IList<IList<int>> expected)
         {
             var actual = new Solution().ThreeSum(nums);
 
@@ -34,7 +35,7 @@ namespace LeetCode_15_3Sum
         public void test_nums_0_0_0()
         {
             var nums = new int[] { 0, 0, 0 };
-            var expected = new List<List<int>>
+            var expected = new List<IList<int>>
                 {new List<int>() {0, 0, 0}};
 
             ShouldBeEqual(nums, expected);
@@ -45,7 +46,20 @@ namespace LeetCode_15_3Sum
     {
         public IList<IList<int>> ThreeSum(int[] nums)
         {
-            return null;
+            var result = new List<IList<int>>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    for (int k = j + 1; k < nums.Length; k++)
+                    {
+                        result.Add(new List<int> { nums[i], nums[j], nums[k] });
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
